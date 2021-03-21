d43f8e5998a1d0d04bbc4e58821bc67d048e704f
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-03-21 12:18:29 -0400

    issue fixed - onward to book status page

--- OnlineBookstore/Bookstore.Web/Site1.Master.designer.cs
+++ OnlineBookstore/Bookstore.Web/Site1.Master.designer.cs
@@ -24,13 +24,13 @@ namespace Bookstore.Web
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// form1 control.
+        /// container control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm container;
 
         /// <summary>
         /// viewBooksLBtn control.
