ab544c25817d2e73557153e56da6529d8c7c68a1
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-03-26 05:32:43 -0400

    New bugs - an issue with postback

--- OnlineBookstore/Bookstore.Web/userprofile.aspx.designer.cs
+++ OnlineBookstore/Bookstore.Web/userprofile.aspx.designer.cs
@@ -15,13 +15,13 @@ namespace Bookstore.Web
     {
 
         /// <summary>
-        /// Label1 control.
+        /// accountStatusLbl control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.Label accountStatusLbl;
 
         /// <summary>
         /// fullNameTxtBx control.
