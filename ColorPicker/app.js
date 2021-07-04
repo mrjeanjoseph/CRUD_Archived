de80b2c8b119f646ebc0658ac58bf6ec7d966555
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-04 13:59:33 -0400

    issue3 fixed - moved on to hex dom

--- ColorPicker/app.js
+++ ColorPicker/app.js
@@ -4,5 +4,13 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener('click', function() {
-    //getting a random number between 0 - 5
+    //getting a random number between 0 - lenght of array
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber)
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber ];
     });
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
