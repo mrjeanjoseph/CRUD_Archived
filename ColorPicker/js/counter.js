25a471feb9641d18ac288b5fc202d478f03d09a4
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-04 19:06:42 -0400

    issue with the event listener

---
+++ ColorPicker/js/counter.js
@@ -0,0 +1,15 @@
+// set inital count
+
+let count = 0;
+
+//Select value and buttons
+const value = document.querySelector('#value');
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    //The 'e' is the event object
+    btn.addEventListener("click", function(e){
+        const styles (e.currentTarget.classList);
+    })
+})
