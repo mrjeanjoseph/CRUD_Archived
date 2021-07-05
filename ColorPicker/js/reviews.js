7445309fa8608c006df5ae1d817cf9a8cfec1217
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-04 22:21:01 -0400

    last commit for the day

---
+++ ColorPicker/js/reviews.js
@@ -0,0 +1,19 @@
+// In memory data object
+
+const reviews = [
+    {
+        id: 1,
+        name: "Veleenah Jean-Joseph",
+        job: "District Attoney",
+        img: "img\img1.jpg",
+        review: 
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam nam similique ab inventore, ullam iste. Et voluptatibus maxime hic, unde aspernatur incidunt consequuntur tempora vel repudiandae est accusamus id asperiores."
+    },{
+        id: 1,
+        name: "Veleenah Jean-Joseph",
+        job: "District Attoney",
+        img: "img\img1.jpg",
+        review: 
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam nam similique ab inventore, ullam iste. Et voluptatibus maxime hic, unde aspernatur incidunt consequuntur tempora vel repudiandae est accusamus id asperiores."
+    }
+]
