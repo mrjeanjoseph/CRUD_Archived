128d53dbe169dd27f4b382aadc5819504b4ce13e
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-05 06:59:57 -0400

    proj reviews completed

--- ColorPicker/js/reviews.js
+++ ColorPicker/js/reviews.js
@@ -12,28 +12,28 @@ const reviews = [
         id: 2,
         name: "Casteeleaux Jean-Joseph",
         job: "Astronomers",
-        img: "img\img1.jpg",
+        img: "https://media.istockphoto.com/photos/group-of-children-smiling-and-looking-at-the-camera-diversity-picture-id1270473756?s=612x612",
         review: 
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam nam similique ab inventore, ullam iste. Et voluptatibus maxime hic, unde aspernatur incidunt consequuntur tempora vel repudiandae est accusamus id asperiores."
     },{
         id: 3,
         name: "Devereaux Jean-Joseph",
         job: "Cheif Information Officer",
-        img: "img\img1.jpg",
+        img: "https://images.unsplash.com/photo-1519238263530-99bdd11df2ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjV8fGtpZHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
         review: 
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam nam similique ab inventore, ullam iste. Et voluptatibus maxime hic, unde aspernatur incidunt consequuntur tempora vel repudiandae est accusamus id asperiores."
     },{
         id: 4,
         name: "Louna Jean-Joseph",
         job: "Finance Director",
-        img: "img\img1.jpg",
+        img: "https://images.unsplash.com/photo-1620592465797-41051bda5733?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fGtpZHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
         review: 
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam nam similique ab inventore, ullam iste. Et voluptatibus maxime hic, unde aspernatur incidunt consequuntur tempora vel repudiandae est accusamus id asperiores."
     },{
         id: 5,
         name: "Denzel Jean-Joseph",
         job: "US Senator",
-        img: "img\img1.jpg",
+        img: "https://images.unsplash.com/photo-1605338655673-a82078d64ed5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fGtpZHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
         review: 
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam nam similique ab inventore, ullam iste. Et voluptatibus maxime hic, unde aspernatur incidunt consequuntur tempora vel repudiandae est accusamus id asperiores."
     }
@@ -71,5 +71,22 @@ function showPerson(person){
 
 nextBtn.addEventListener('click', function(){
     currentItem++;
+    if(currentItem > reviews.length-1){
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click', function(){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
+
+randomBtn.addEventListener('click', function() {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    //console.log(currentItem);
     showPerson(currentItem);
 })
