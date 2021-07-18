bbdf50e14dad11d12521983b81e52972756ebff0
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-18 11:37:35 -0400

    CRUD create and read added

--- TodoProj/index.js
+++ TodoProj/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 app.use("/static", express.static("public"));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.set("useFindAndModify", false);
 
@@ -22,10 +22,20 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
 app.set("view engine", "ejs");
 
 
-app.get('/',(req, res) => {
-    res.render('todo.ejs');
+app.get('/', (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+        res.render("todo.ejs", { todoTasks: tasks });
+    });
 });
 
-app.get('/',(req, res) => {
-    console.log(req.body);
+app.post('/', async (req, res) => {
+    const todoTask = new TodoTask({
+        content: req.body.content
+    });
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.redirect("/");
+    }
 });
