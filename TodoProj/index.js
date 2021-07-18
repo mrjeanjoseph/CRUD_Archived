31e461174f25a668a883c62e038ad53c20ff2df7
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-18 10:30:53 -0400

    last issue fixed - MongoDb online

--- TodoProj/index.js
+++ TodoProj/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
-const dotenv = require(“dotenv”);
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({ extended: true}));
+
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+    console.log("Connected to db!");
+
+    app.listen(3000, () => console.log("Server Up and running"));
+})
 
 app.set("view engine", "ejs");
 
@@ -18,5 +27,3 @@ app.get('/',(req, res) => {
 app.get('/',(req, res) => {
     console.log(req.body);
 });
-
-app.listen(3000, () => console.log("Server Up and running"));
