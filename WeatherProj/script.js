879dc7033be6bf30d0e29039a9e41db2d8ddc7ab
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-18 16:40:11 -0400

    issue1 Uncaught (in promise) SyntaxError: Unexpected token < in JSON at position 0

---
+++ WeatherProj/script.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", () => {
+    let lon;
+    let lat;
+    let zipCode;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(longLatResult => {
+            lon = longLatResult.coords.longitude;
+            lat = longLatResult.coords.latitude;
+
+            let apiKey = '8f886488638f4ae9b49d7a6c1c840c2f';
+            // const apiUrl = `api.openweathermap.org/data/2.5/weather?${lat}&${lon}&appid=${apiKey}`;
+            const apiUrl = `api.openweathermap.org/data/2.5/weather?zip=27610,1&appid=${apiKey}`;
+
+            fetch(apiUrl)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            });
+        });
+    }
+});
