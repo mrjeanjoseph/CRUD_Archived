d33d5ee79f84a0a4a56394e56abb9d8eb730da83
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-18 09:41:45 -0400

    issue2 - difficulties setting up MongoDb

--- TodoProj/index.js
+++ TodoProj/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const dotenv = require(“dotenv”);
 
-app.get('/',(req, res) => {
-    res.send('Hello World!');
-    });
+dotenv.config();
 
-app.listen(3000, () => console.log("Server Up and running"));
+app.use("/static", express.static("public"));
+
+app.use(express.urlencoded({ extended: true}));
 
 app.set("view engine", "ejs");
+
+
+app.get('/',(req, res) => {
+    res.render('todo.ejs');
+});
+
+app.get('/',(req, res) => {
+    console.log(req.body);
+});
+
+app.listen(3000, () => console.log("Server Up and running"));
