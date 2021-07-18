44a995cfe944075a62ccc2bc0f0220dda80bc717
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-18 13:32:38 -0400

    Cannot read property 'forEach' of undefined error

--- TodoProj/index.js
+++ TodoProj/index.js
@@ -19,15 +19,19 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
     app.listen(3000, () => console.log("Server Up and running"));
 })
 
+
+//VIEW ENGINE CONFIG
 app.set("view engine", "ejs");
 
-
+// GET METHOD
 app.get('/', (req, res) => {
     TodoTask.find({}, (err, tasks) => {
         res.render("todo.ejs", { todoTasks: tasks });
     });
 });
 
+
+// POST METHOD
 app.post('/', async (req, res) => {
     const todoTask = new TodoTask({
         content: req.body.content
@@ -38,4 +42,33 @@ app.post('/', async (req, res) => {
     } catch (err) {
         res.redirect("/");
     }
+});
+
+
+//UPDATE
+app
+    .route("/edit/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+
+        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
+    });
+
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
 });
