b66d0d121b7e4001885057cfe6b961f7456c6326
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-19 10:02:03 -0400

    issue3 'uncaught typeError'

--- WeatherProj/script.js
+++ WeatherProj/script.js
@@ -10,19 +10,22 @@ window.addEventListener("load", () => {
             lon = longLatResult.coords.longitude;
             lat = longLatResult.coords.latitude;
 
-            let apiKey = '8f886488638f4ae9b49d7a6c1c840c2f';
-            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lon }&appid=${ apiKey }`;
+            // let apiKey = '8f886488638f4ae9b49d7a6c1c840c2f';
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lon }&appid=${ apiKey }`;
+            const apiurl2 = `${ proxy }https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1${lat},${lon}`;
 
-            fetch(apiUrl)
+
+           fetch(apiurl2)
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
                     console.log(data);
-                    const { temperature, summary } = data.currently
+                     const { temperature, summary } = data.currently;
                 });
-                temperatureDegree.textContent = temperature;
-                temperatureDescription.textContent = summary;
+                // temperatureDegree.textContent = temperature;
+                // temperatureDescription.textContent = summary;
         });
     }
 });
