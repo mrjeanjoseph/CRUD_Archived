adc86e144cfac4db20c7a1cbc95fffcb225f5b36
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-11-24 16:00:00 -0500

    error in main js file

---
+++ mem-game/app.js
@@ -0,0 +1,82 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card options
+    const cardArray = [
+        {
+            name: "fries",
+            img: "images/fries.png"
+        },
+        {
+            name: "fries",
+            img: "images/fries.png"
+        },
+        {
+            name: "cheeseburger",
+            img: "images/cheeseburger.png"
+        },
+        {
+            name: "cheeseburger",
+            img: "images/cheeseburger.png"
+        },
+        {
+            name: "hotdog",
+            img: "images/hotdog.png"
+        },
+        {
+            name: "hotdog",
+            img: "images/hotdog.png"
+        },
+        {
+            name: "ice-cream",
+            img: "images/ice-cream.png"
+        },
+        {
+            name: "ice-cream",
+            img: "images/ice-cream.png"
+        },
+        {
+            name: "milkshake",
+            img: "images/milkshake.png"
+        },
+        {
+            name: "milkshake",
+            img: "images/milkshake.png"
+        },
+        {
+            name: "pizza",
+            img: "images/pizza.png"
+        },
+        {
+            name: "pizza",
+            img: "images/pizza.png"
+        },
+    ]
+
+    const grid = document.querySelector('.grid');
+    var cardsChosen = [];
+    var cardsChosenId = [];
+
+    //Create the
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click',flipCard);
+            grid.appendChild(card);
+        }
+    }
+
+    function flipCard() {
+        var cardId = this.getAttribute('data-id');
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
+        if (cardsChosen.length === 2) {
+            setTimeout.apply(checkForMatch, 500)
+        }
+    }
+    createBoard();
+})
