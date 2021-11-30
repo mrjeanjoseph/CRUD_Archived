8c8e2a2dcfde23da200d8f836886a2bdad63cceb
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-11-30 14:05:45 -0500

    shooters and invaders implemented

---
+++ space-invade/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const squares = document.querySelectorAll('.grid div');
+    const resultDisplay = document.querySelector('#result');
+
+    let width = 15;
+    let currentShooterIndex = 202;
+    let currentInvaderIndex = 0;
+    let alienInvadersTakenDown = [];
+    let result = 0;
+    let direction = 1;
+    let invaderId;
+
+    const alienInvaders = [
+        0,1,2,3,4,5,6,7,8,9,
+        15,16,17,18,19,20,21,22,23,24,
+        30,31,32,33,34,35,36,37,38,39
+    ]
+
+    //define the alien invaders
+    alienInvaders.forEach(invader => squares[
+        currentInvaderIndex + invader
+    ].classList.add('invader'));
+
+    //draw the shooter
+    squares[
+        currentShooterIndex
+    ].classList.add('shooter');
+})
