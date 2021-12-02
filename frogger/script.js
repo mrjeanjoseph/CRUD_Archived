9aa7bfc9e1c07059faa46331e8d397ce8cad8b2d
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-01 20:42:27 -0500

    issue fixed - adding rules to win

--- frogger/script.js
+++ frogger/script.js
@@ -1,33 +1,142 @@
 document.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('.grid div');
-    const timeLeft = document.querySelector('#time-left');
-    const result = document.querySelector('#result');
-    const startBtn = document.querySelector('#button');
+    const timeLeft = ocument.querySelectorAll('#time-left');
+    const result = document.querySelectorAll('#result');
+    const startBtn = document.querySelectorAll('#button');
+    //init the obstacles
+    const carsLeft = document.querySelectorAll('.car-left');
+    const carsRight = document.querySelectorAll('.car-right');
+    const logsLeft = document.querySelectorAll('.log-left');
+    const logsRight = document.querySelectorAll('.log-right');
+    //env access
     const width = 9;
-    const currentIndex = 76;
+    let currentIndex = 76;
     let timerId;
 
-    //redender frog on starting block
+    //Get the frog moving
     squares[currentIndex].classList.add('frog');
 
-    //A function that will move the frog
-    function moveFrog(e){
+    //the function that will get the frog moving
+    function moveFrog(e) {
         squares[currentIndex].classList.remove('frog');
-        switch(e.keyCode) {
+        switch (e.keyCode) {
             case 37:
-                if(currentIndex % width !==0) currentIndex -=1;
+                if (currentIndex % width !== 0) currentIndex -= 1;
                 break;
             case 38:
-                if(currentIndex -width >= 0) currentIndex -= width;
+                if (currentIndex - width >= 0) currentIndex -= width;
                 break;
             case 39:
-                if(currentIndex % width < width -1) currentIndex +=1;
+                if (currentIndex % width < width - 1) currentIndex += 1;
                 break;
             case 40:
-                if(currentIndex + width < width * width) currentIndex += width;
-                break;
+                if (currentIndex + width < width * width) currentIndex += width;
+                break
         }
         squares[currentIndex].classList.add('frog');
-        lo
+        lose();
+        win();
     }
-});
+
+    //move cars
+    function autoMoveCars() {
+        carsLeft.forEach(CL => moveCarLeft(CL));
+        carsRight.forEach(CR => moveCarLeft(CR));
+    }
+
+    //On a timeloop, move cars left
+    function moveCarLeft(CL) {
+        switch (true) {
+            case CL.classList.contains('c1'):
+                CL.classList.remove('c1');
+                CL.classList.add('c2');
+                break;
+            case CL.classList.contains('c2'):
+                CL.classList.remove('c2');
+                CL.classList.add('c3');
+                break;
+            case CL.classList.contains('c3'):
+                CL.classList.remove('c3');
+                CL.classList.add('c1');
+                break;
+        }
+    }
+
+    //On a timeloop, move cars left
+    function moveCarLeft(CR) {
+        switch (true) {
+            case CR.classList.contains('c1'):
+                CR.classList.remove('c1');
+                CR.classList.add('c3');
+                break;
+            case CR.classList.contains('c2'):
+                CR.classList.remove('c2');
+                CR.classList.add('c1');
+                break;
+            case CR.classList.contains('c3'):
+                CR.classList.remove('c3');
+                CR.classList.add('c2');
+                break;
+        }
+    }
+
+    //move logs
+    function autoMoveLogs() {
+        logsLeft.forEach(LL => moveCarLeft(LL));
+        logsRight.forEach(LR => moveCarLeft(LR));
+    }
+
+    //On a timeloop, move logs left
+    function moveLogLeft(LL) {
+        switch (true) {
+            case LL.classList.contains('l1'):
+                LL.classList.remove('l1');
+                LL.classList.add('l2');
+                break;
+            case LL.classList.contains('l2'):
+                LL.classList.remove('l2');
+                LL.classList.add('l3');
+                break;
+            case LL.classList.contains('l3'):
+                LL.classList.remove('l3');
+                LL.classList.add('l1');
+                break;
+            case LL.classList.contains('l4'):
+                LL.classList.remove('l4');
+                LL.classList.add('l5');
+                break;
+            case LL.classList.contains('l5'):
+                LL.classList.remove('l5');
+                LL.classList.add('l1');
+                break;
+        }
+    }
+
+    //On a timeloop, move log right
+    function moveLogRight(LR) {
+        switch (true) {
+            case LR.classList.contains('l1'):
+                LR.classList.remove('l1');
+                LR.classList.add('l5');
+                break;
+            case LR.classList.contains('l2'):
+                LR.classList.remove('l2');
+                LR.classList.add('l1');
+                break;
+            case LR.classList.contains('l3'):
+                LR.classList.remove('l3');
+                LR.classList.add('l2');
+                break;
+            case LR.classList.contains('l4'):
+                LR.classList.remove('l4');
+                LR.classList.add('l3');
+                break;
+            case LR.classList.contains('l5'):
+                LR.classList.remove('l5');
+                LR.classList.add('l4');
+                break;
+        }
+    }
+
+    //rules
+})
