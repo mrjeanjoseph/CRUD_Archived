0a8cc52657457a35f1030495c06888abacf298ab
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-02 21:22:25 -0500

    reading data from the display

---
+++ html-crud/script.js
@@ -0,0 +1,17 @@
+
+function onFormSubmit(){
+    var formData = readFormData();
+}
+
+function readFormData(){
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["empCode"] = document.getElementById("empCode").value;
+    formData["salary"] = document.getElementById("salary").value;
+    formData["city"] = document.getElementById("city").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    
+}
