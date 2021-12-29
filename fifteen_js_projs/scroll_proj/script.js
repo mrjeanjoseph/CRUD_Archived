baaf004ba53f7a8b4db1c93a19a82459364666e8
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-29 08:11:26 -0500

    positioning the navbar dynamically

--- fifteen_js_projs/scroll_proj/script.js
+++ fifteen_js_projs/scroll_proj/script.js
@@ -23,3 +23,24 @@ navToggle.addEventListener("click", function () {
     }
 })
 
+const navbar = document.querySelector(".nav");
+const topLink = document.querySelector(".top-link");
+// working with the nav bar
+window.addEventListener("scroll", function(){
+    // console.log(window.pageYOffset);
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+
+    if(scrollHeight > 50) {
+        topLink.classList.add("show-link");
+    }else {
+        topLink.classList.remove("show-link");
+    }
+
+});
+
