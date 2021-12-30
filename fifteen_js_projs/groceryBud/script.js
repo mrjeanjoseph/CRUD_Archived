8f7e5720d3a4ac9e3fe93f29055a2eb83c94e9fe
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-30 18:06:35 -0500

    issue DOM not responding to element

--- fifteen_js_projs/groceryBud/script.js
+++ fifteen_js_projs/groceryBud/script.js
@@ -13,6 +13,7 @@ let editId = "";
 
 form.addEventListener("submit", addItem); //Submitting the form
 clearBtn.addEventListener("click", clearItems);
+window.addEventListener("DOMContentLoaded", loadItemsFromLocalStorage)
 
 function addItem(e) {
     e.preventDefault();
@@ -23,35 +24,18 @@ function addItem(e) {
     // console.log(value)
     if (value && !editFlag) {
         // console.log("Adding items");
-        const element = document.createElement("article");
-        element.classList.add("grocery-item"); // adding the class
-        const attr = document.createAttribute("data-id"); // adding the id attr
-        attr.value = id;
-        element.setAttributeNode(attr);
-        element.innerHTML = `
-        <p class="title">${value}</p>
-        <div class="btn-container">
-            <button type="button" class="edit-btn">
-                <i class="fas fa-edit"></i>
-            </button>
-            <button type="button" class="delete-btn">
-                <i class="fas fa-trash"></i>
-            </button>
-        </div>`;
-
-        const deleteBtn = element.querySelector(".delete-btn");//access the delete class inside the element
-        const editBtn = element.querySelector(".edit-btn");//access the edit class inside the element
-
-        deleteBtn.addEventListener("click", deleteItem);
-        editBtn.addEventListener("click", editItem);
-        list.appendChild(element); // this is to append the html items above
+        createListItem(id, value);
         displayAlert("Item added to the list", "success"); // on success display message
         container.classList.add("show-container") // make container visible on success
 
         addToLocalStorage(id, value); // This will save our data in local mem
         setBackToDefault(); //This will clear out local mem
     } else if (value !== "" && editFlag === true) {
-        console.log("editing");
+        // console.log("editing");
+        editElement.innerHTML = value;
+        displayAlert("value changed", "success");
+        //editLocalStorage(editId, value);
+        setBackToDefault();
     } else {
         // console.log("empty value")
         // alert.textContent = "Please enter a value";
@@ -68,10 +52,6 @@ function displayAlert(text, action) { // turn alert into a function to avoid dup
         alert.textContent = "";
         alert.classList.remove(`alert-${action}`);
     }, 3000)
-}
-
-function addToLocalStorage(id, value) {
-    console.log("back to default")
 }
 
 function setBackToDefault() {
@@ -96,9 +76,108 @@ function clearItems() { //Clearing the item list
     localStorage.removeItem("list")
 }
 
-function deleteItem(){
-    
+function deleteItem(e) {
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id;
+    list.removeChild(element);
+    if (list.children.lenght === 0) {
+        container.classList.remove("show-container");
+    }
+    displayAlert("Item has been removed", "danger");
+    setBackToDefault();
+    // remove from local storage
+    removeFromLocalStorage(id);
 }
-function editItem(){
-    displayAlert("Item has been edited", "warning");
+
+function editItem(e) {
+    const element = e.currentTarget.parentElement.parentElement;
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+    grocery.value = editElement.innerHTML;
+    editFlag = true;
+    editId = element.dataset.id;
+    submitBtn.textContent = 'edit'
+
+    displayAlert("Item is being edited", "danger");
 }
+
+
+
+function addToLocalStorage(id, value) {
+    // console.log("back to default")
+    const grocery = { id: id, value: value }; // this can shorten like this {id, value}
+    // console.log(grocery);
+    let items = getLocalStorage();
+    console.log(items)
+    items.push(grocery);
+    localStorage.setItem("list", JSON.stringify(items));
+    // console.log(items);
+
+}
+
+function getLocalStorage() {
+    return localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : [];
+}
+
+function removeFromLocalStorage(id) {
+    let items = getLocalStorage();
+    items = items.filter(function (item) {
+        if (item.id !== id) {
+            return item;
+        }
+    });
+    localStorage.setItem("list", JSON.stringify(items));
+}
+
+function editLocalStorage(id, value) {
+    let items = getLocalStorage();
+    items = items.map(function (item) {
+        if (item.id === id) {
+            item.value = value;
+        }
+        return item;
+    });
+    localStorage.setItem("list", JSON.stringify(items));
+}
+
+function loadItemsFromLocalStorage() {
+    let items = getLocalStorage();
+    if (items.lenght > 0) {
+        items.forEach(function (item) {
+            createListItem(item.id, item.value)
+        });
+        container.classList.add("show-container");
+    }
+}
+
+function createListItem(id, value) {
+    const element = document.createElement("article");
+    element.classList.add("grocery-item"); // adding the class
+    const attr = document.createAttribute("data-id"); // adding the id attr
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `
+    <p class="title">${value}</p>
+    <div class="btn-container">
+        <button type="button" class="edit-btn">
+            <i class="fas fa-edit"></i>
+        </button>
+        <button type="button" class="delete-btn">
+            <i class="fas fa-trash"></i>
+        </button>
+    </div>`;
+
+    const deleteBtn = element.querySelector(".delete-btn");//access the delete class inside the element
+    const editBtn = element.querySelector(".edit-btn");//access the edit class inside the element
+
+    deleteBtn.addEventListener("click", deleteItem);
+    editBtn.addEventListener("click", editItem);
+
+    list.appendChild(element); // this is to append the html items above
+}
+
+// localStorage.setItem("orange", JSON.stringify(["item", "item2"]));
+// const oranges = JSON.parse(localStorage.getItem("orange"));
+// console.log(oranges)
+
+// localStorage.removeItem("orange");
+// console.log(oranges);
