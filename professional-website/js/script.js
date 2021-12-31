138dc4ecdf77cbd3f990b38eca66cab844cbf35c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-31 18:01:19 -0500

    completed nav and header

---
+++ professional-website/js/script.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop();
+        // console.log(scroll);
+        if(scroll > 50) {
+            $(".sticky").addClass("stickyadd")
+        } else {
+            $(".sticky").removeClass("stickyadd")
+        }
+    });
+
+    var typed = new Typed(".element", {
+        strings: ["Jean-Joseph", "a fullstack developer", "an entrepreneur"],
+        smartBackspace: true,
+        typeSpeed:50,
+        backSpeed:200,
+        loop:true,
+        loopCount:Infinity,
+        startDelay:1000
+    });
+});
