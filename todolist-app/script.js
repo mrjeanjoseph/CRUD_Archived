fce3f6291d24f52954d2e30cf45a5957f8e7ea3a
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-03 17:59:32 -0500

    added the tab functionality

--- todolist-app/script.js
+++ todolist-app/script.js
@@ -1,24 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function () {
     createToDoUI();
     addProject();
 })
 
-function createToDoUI(){
+function createToDoUI() {
     $("#projects").tabs();
-    
-    $("ul").sortable({axis:"x", containment:"#projects"});
-    $("ol").sortable({axis:"y", containment:"#projects"});
+
+    $("ul").sortable({ axis: "x", containment: "#projects" });
+    $("ol").sortable({ axis: "y", containment: "#projects" });
 }
 
 function addProject() {
-    $("#btnAddProject").button()
-    .click(function () {
+    $("#btnAddProject").button().click(function () {
+
         $("#project-dialog").dialog({
-            width:400,
-            resizable:false,
-            modal:true,
-            buttons:{
-                
+            width: 400,
+            resizable: false,
+            modal: true,
+            buttons: {
+                "Add new project": function () {
+                    var projectName = $("#new-project").val();
+                    $(`<li><a href="#${projectName}">${projectName}</a></li>`)
+                        .appendTo("#main");
+
+                    $(`<ol id=${projectName}></ol>`).appendTo("#projects");
+                    var tabCount = $("#projects .ui-tabs-nav li").length;
+                    $("#projects").tabs("option", "active", tabCount - 1);
+
+                    $("#projects").tabs("refresh");
+                    $("#new-project").val("");
+                    $(this).dialog("close");
+                },
+                "Cancel": function () {
+                    $("#projects").tabs("refresh");
+                    $("#new-project").val("");
+                    $(this).dialog("close");
+                }
             }
         })
     });
