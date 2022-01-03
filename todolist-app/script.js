b111d307639a7ebd92e6f83d53d4f7ff668d410b
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-03 13:23:57 -0500

    wiring up the add task btn

--- todolist-app/script.js
+++ todolist-app/script.js
@@ -2,6 +2,28 @@ $(document).ready(function () {
     $("#projects").tabs();
     $("ul").sortable({ axis: "x", containment: "#projects" });
     $("ol").sortable({ axis: "y", containment: "#projects" });
+    $("#btnAddTask").button().click(function(){    
+        $("#task-dialog").dialog({
+            width: 400,
+            resizable: false,
+            modal: true,
+            buttons: {
+                "Add new task": function() {
+                    $("#projects").tabs("refresh");
+                    var activeTab = $("#projects").tabs("option","active");
+                    // alert(activeTab);
+                    var title = $(`#main > li:nth-child(${(activeTab + 1)}) > a`).attr("href");
+                    alert(title);
+                    $(this).val("");
+                    $(this).dialog("close");
+                },
+                "Cancel": function() {                    
+                    $(this).val("");
+                    $(this).dialog("close");
+                }
+            }
+        })
+    });
     $("#btnAddProject").button().click(function () {
         $("#project-dialog").dialog({
             width: 400,
@@ -32,4 +54,3 @@ $(document).ready(function () {
     // var theColorIs = $('#ui-id-1').css("background-color");
     // console.log(theColorIs);
 })
-
