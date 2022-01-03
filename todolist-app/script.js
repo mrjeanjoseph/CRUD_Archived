545f147050746a556650fd0cb67592d5633023d4
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-03 09:12:27 -0500

    creating the dialog box

--- todolist-app/script.js
+++ todolist-app/script.js
@@ -3,7 +3,18 @@ $(document).ready(function() {
     $("ul").sortable({axis:"x", containment: "#projects"});
     $("ol").sortable({axis:"y", containment: "#projects"});
     $("#btnAddProject").button().click(function() {
-        $("#project-dialog").dialog();
+        $("#project-dialog").dialog({
+            width:400, 
+            resizable:false,
+            modal:true,
+            buttons:{
+                "Add new project": function() {},
+                "Cancel":function(){
+                    // $(this).val("");
+                    $(this).dialog("close");
+                },    
+            }
+        });
     });
 
     // var theColorIs = $('#ui-id-1').css("background-color");
