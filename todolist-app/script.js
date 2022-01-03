929d659f76ee38de8934947a7cf0b60c567510cc
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-03 12:43:14 -0500

    working jq ui logic

--- todolist-app/script.js
+++ todolist-app/script.js
@@ -1,25 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#projects").tabs();
-    $("ul").sortable({axis:"x", containment: "#projects"});
-    $("ol").sortable({axis:"y", containment: "#projects"});
-    $("#btnAddProject").button().click(function() {
+    $("ul").sortable({ axis: "x", containment: "#projects" });
+    $("ol").sortable({ axis: "y", containment: "#projects" });
+    $("#btnAddProject").button().click(function () {
         $("#project-dialog").dialog({
-            width:400, 
-            resizable:false,
-            modal:true,
-            buttons:{
-                "Add new project": function() {
+            width: 400,
+            resizable: false,
+            modal: true,
+            buttons: {
+                "Add new project": function () {
                     var projectName = $("#new-project").val();
                     $(`<li><a href="#${projectName}">${projectName}</a></li>`)
-                    .appendTo("#main");
+                        .appendTo("#main");
                     $("#projects").tabs("refresh");
+
+
+                    $(`<ol id="${projectName}"></ol>`).appendTo("#projects");
+                    var tabCount = $("#projects .ui-tabs-nav li").length;
+                    $("#projects").tabs("option", "active", tabCount - 1);
                     $(this).val("");
                     $(this).dialog("close");
                 },
-                "Cancel":function(){
+                "Cancel": function () {
                     $(this).val("");
                     $(this).dialog("close");
-                },    
+                },
             }
         });
     });
