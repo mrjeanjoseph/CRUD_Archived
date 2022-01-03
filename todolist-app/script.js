262cbccfdd01244b71089daa91e2c5459f25e34f
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-03 17:02:09 -0500

    implement add proj using jq ui

--- todolist-app/script.js
+++ todolist-app/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     createToDoUI();
+    addProject();
 })
 
 function createToDoUI(){
@@ -7,4 +8,18 @@ function createToDoUI(){
     
     $("ul").sortable({axis:"x", containment:"#projects"});
     $("ol").sortable({axis:"y", containment:"#projects"});
+}
+
+function addProject() {
+    $("#btnAddProject").button()
+    .click(function () {
+        $("#project-dialog").dialog({
+            width:400,
+            resizable:false,
+            modal:true,
+            buttons:{
+                
+            }
+        })
+    });
 }
