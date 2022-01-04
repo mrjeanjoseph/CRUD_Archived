8e1d6f5f9366db60537ed96359374ebef1c99db2
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-04 12:55:56 -0500

    server and front-end completed

--- read-csv-data/assets/script.js
+++ read-csv-data/assets/script.js
@@ -1,2 +1,23 @@
-// Making sure this works properly
-// alert("Hello JavaScript")
+// This is the frond end
+
+// $(() =>{
+//     console.log( "ready!" );
+// })
+
+$(document).ready(function() {
+    $("form").submit(function(e){
+        e.preventDefault();
+        var mytext = $("mytext").val();
+        $.ajax({
+            url:"/ajaxdemo",
+            data: {text:mytext},
+            method:"POST",
+            contentType:"application/x-www-form-urlencoded",
+            success:function(res){
+                alert(res.from)
+            }, error: function(err){
+                console.log(err);
+            }
+        })
+    })
+ })
