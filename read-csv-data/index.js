87f585cf182bc9799c268c4a85f77973e3d3107c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-04 12:22:41 -0500

    setting up the server

---
+++ read-csv-data/index.js
@@ -0,0 +1,25 @@
+//This is the backend server
+const express = require('express');
+const bodyParser = require("body-parser");
+const path = require("path");
+const httpMsgs = require("http-msgs");
+
+const app = require("path");
+
+const app = express();
+app.listen(2022);
+app.use(bodyparser.urlencoded({ extended:false}));
+
+app.use(express.static("assets"));
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/ajaxdemo", function(req, res) {
+    var data = req.body;
+    console.log(data);
+
+    httpMsgs.sendJSON(req, res, {
+        from : "server"
+    });
+});
