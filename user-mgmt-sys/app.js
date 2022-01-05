370a3a83cff85982c9b87ba135802c1dc34990f2
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-04 20:50:07 -0500

    mysql added

--- user-mgmt-sys/app.js
+++ user-mgmt-sys/app.js
@@ -1,15 +1,34 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 2022;
 
 // const port = 5000;
 
+//parsing middleware configuration
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//parse application/json
+app.use(bodyParser.json());
+
+//static Files
+app.use(express.static('public'));
+
+//templating engine
+app.engine('hbs', engine({extname:'.hbs', defaultLayout: "main"}));
+app.set('view engine', 'hbs');
+
+app.get('', function(req, res) {
+    res.render('home');
+})
 
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log(`Listening on port ${port}`);
 })
