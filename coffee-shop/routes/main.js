191e4cfff23e938f2c8a081a7591f8dae8015e50
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-09 09:01:27 -0500

    separate files

--- coffee-shop/routes/main.js
+++ coffee-shop/routes/main.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send('this is the main page again!');
+    const data = {
+        title: "kafe Kiskeya ou Boyo!",
+        deskripsyon: "Nou apresye ou menm avek fanmi/zanmi'w vinn dine nan restaurant sa a. Mwen swete nou jwen yon bon ekperyans." 
+    }
+
+    res.render('homepage', data);
 })
 
 module.exports = router;
