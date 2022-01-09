0f7b3e46955ebae4773035fc5558f2bd555ded5d
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-09 08:12:48 -0500

    starting the proj

--- coffee-shop/routes/main.js
+++ coffee-shop/routes/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send('this is the main page');
+    res.send('this is the main page again!');
 })
 
 module.exports = router;
