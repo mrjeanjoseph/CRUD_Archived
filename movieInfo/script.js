b2a8016d612e9f878e7df37f3aaeb1d021ddc051
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-10 08:32:57 -0500

    getting the movie list

---
+++ movieInfo/script.js
@@ -0,0 +1,69 @@
+$(document).ready(() => {
+    $("#searchForm").on("submit", (e) =>{
+        
+        let searchText = $("#searchText").val();
+        getMovies(searchText);
+
+        e.preventDefault();
+    });
+});
+
+//`http://www.omdbapi.com?s=${searchText}`
+//"http://www.omdbapi.com?s=+searchText"
+//`http://www.omdbapi.com/?t=home`
+//http://www.omdbapi.com/?i=tt3896198&apikey=93974bd3
+//http://www.omdbapi.com/?apikey=[93974bd3]&s=${searchText}
+//
+
+function getMovies(searchText) {
+     axios.get(`http://www.omdbapi.com/?s=${searchText}&apikey=93974bd3`)
+     .then(function(res) {
+         let movies = res.data.Search;
+         console.log(movies);
+         let output = '';
+         $.each(movies, function (index, movie){
+            output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${movie.Poster}">
+                        <h5>${movie.Title}</h5>
+                        <a onclick="movieSelected('${movie.imdbID}')" 
+                            class="btn btn-primary href="#">Movie Details</a>
+                    </div>
+                </div>
+            `;
+         });
+         $("#movies").html(output);
+     })
+     .catch(function(err){
+        console.log(err)
+     });
+}
+
+function movieSelected(id) {
+    sessionStorage.setItem("movieId", id);
+    window.location = "movie.html";
+    return false;
+}
+
+function getMovie() {
+    let movieId = sessionStorage.getItem("movieId");
+
+    axios.get(`http://www.omdbapi.com/?i=${movieId}&apikey=93974bd3`)
+    .then(function(res) {
+        console.log(res);
+        let movie = res.data;
+
+        let output = `
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="${movie.Poster}">
+                </div>
+            </div>
+        `;
+        $("#movie").html(output);
+    })
+    .catch(function(err){
+       console.log(err)
+    });
+}
