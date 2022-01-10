db4a13827f405af639279513828057aa7ae3f80e
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-10 09:00:52 -0500

    issue with api key

--- movieInfo/script.js
+++ movieInfo/script.js
@@ -59,6 +59,29 @@ function getMovie() {
                 <div class="col-md-4">
                     <img src="${movie.Poster}">
                 </div>
+                <div class="col-md-8">
+                    <h2>${movie.Title}</h2>
+                        <ul>
+                            <li class="list-group-item"><strong>Genre: </strong>${movie.Genre}</li>
+                            <li class="list-group-item"><strong>Released: </strong>${movie.Released}</li>
+                            <li class="list-group-item"><strong>Rated: </strong>${movie.Rated}</li>
+                            <li class="list-group-item"><strong>IMDB Rating: </strong>${movie.imdbRating}</li>
+                            <li class="list-group-item"><strong>Director: </strong>${movie.Director}</li>
+                            <li class="list-group-item"><strong>Writer: </strong>${movie.Writer}</li>
+                            <li class="list-group-item"><strong>Actors: </strong>${movie.Actors}</li>
+                        </ul>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="well">
+                    <h3>Plot</h3>
+                    ${movie.Plot}
+                    <hr/>
+                    <a href="http://imdb.com/title/${movie.imdbID}"
+                        target="_blank" class="btn btn-primary">View Movie</a>
+                    <a href="index.html" class="btn btn-default">Go back to Search</a>
+                </div>
             </div>
         `;
         $("#movie").html(output);
