9b449f020e7f87b2e330c828ffef9a8a4cbd211a
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-12 08:28:58 -0500

    issue with promise

--- nodejs-file-upload/app.js
+++ nodejs-file-upload/app.js
@@ -24,10 +24,10 @@ app.post('/single', async function (request, response, next) {
         const customFileName = new Date().getTime().toString() + path.extname(file.name);
         const savePath = path.join(__dirname, 'public', 'uploads', customFileName);
 
-        if(file.truncated){
+        if (file.truncated) {
             throw new Error("File size is too big...,");
         }
-        if(file.mimetype !== 'image/jpeg'){
+        if (file.mimetype !== 'image/jpeg') {
             throw new Error("Only jpegs are supported")
         }
         await file.mv(savePath);
@@ -36,6 +36,26 @@ app.post('/single', async function (request, response, next) {
     } catch (error) {
         console.log(error);
         response.send("Error uploading file");
+    }
+})
+
+app.post("/multiple", async function (request, response, next) {
+    try {
+        const files = request.files.mFiles
+        // files.forEach( function(file) {
+        //     const savePath = path.join(__dirname, 'public', 'uploads', file.name);
+        // })
+        // await file.mv(savePath)
+        let promises = [];
+        files.forEach(function (file) {
+            const savePath = path.join(__dirname, 'public', 'uploads', file.name);
+            promises.push(file.mv(savePath));
+        })
+        await Promise.all(promises);
+
+    } catch (error) {
+        console.log(error);
+        response.send("Error uploading the multiple files...,")
     }
 })
 
