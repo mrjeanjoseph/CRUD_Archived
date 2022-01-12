c2915cb984048f62f5ee31a7d49635de6e3580f2
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-12 16:57:23 -0500

    officially cruding

--- nodejs-projs/basictodoapp/public/app.js
+++ nodejs-projs/basictodoapp/public/app.js
@@ -4,10 +4,34 @@ $.ajax({
     // success: function(data){
     //     console.log("success", data)
     // }
-}).then(function(data) {
-    // console.log("std - success", data)
-    for(const id in data) {
-        if(id === 'nextid') return;
-        console.log(data[id].title)
+}).then(function (data) {
+    // console.log("std - success", data);
+    const count = 0;
+    for (const id in data) {
+        //count++;
+        if (id === "nextid") return "";
+        // console.log(data[id].title)
+        // console.log(count);
+
+        $("body").append(`
+            <div>
+                <h3>${data[id].title}</h3>
+                <p>${data[id].content}</p>
+            </div>
+        `);
+
+        $("body").append("Hello")
     }
+})
+
+$("#add-items").click(function (e) {
+    e.preventDefault();
+    const getTitle = $("#add-title").val();
+    const getContent = $("#add-content").val();
+
+    console.log({ getTitle, getContent })
+
+    $("#add-title").val("");
+    $("#add-content").val("");
+
 })
