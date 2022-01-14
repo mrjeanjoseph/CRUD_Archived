b7e79083714b02eb97766ef960cdcda44369b989
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-13 22:25:56 -0500

    connecting to sql

--- nodejs-projs/calendar-node-sql/src/config.js
+++ nodejs-projs/calendar-node-sql/src/config.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const dotenv = require("dotenv");
+const assert = require("assert");
 
 dotenv.config();
 
@@ -20,3 +21,27 @@ const {
 
 const sqlEncrypt = process.env.SQL_ENCRYPT === "true";
 
+assert(PORT, "PORT is required");
+assert(HOST, "HOST is required");
+
+module.exports = {
+    port: PORT,
+    host: HOST,
+    url: HOST_URL,
+    cookiePwd: COOKIE_ENCRYPT_PWD,
+    sql:{
+        server: SQL_SERVER,
+        database: SQL_DATABASE,
+        user:SQL_USER,
+        password:SQL_PASSWORD,
+        options:{
+            encrypt:sqlEncrypt,
+            enableArithAbort: true
+        }
+    },
+    okta: {
+        Url: OKTA_ORG_URL,
+        clientId: OKTA_CLIENT_ID,
+        clientSecret: OKTA_CLIENT_SECRET
+    }
+};
--- nodejs-projs/calendar-node-sql/src/index.js
+++ nodejs-projs/calendar-node-sql/src/index.js
@@ -1,18 +1,19 @@
 "use strict";
 
 const server = require("./server");
+const config = require("./config");
 
 const startServer = async function() {
     try{
-        const config = { 
-            host: "localhost", 
-            port: 2022
-        };
+        // const config = { 
+        //     host: "localhost", 
+        //     port: 2022
+        // };
 
         const app = await server(config);
         await app.start();
 
-        console.log(`Server running at http://${config.host}:${config.port}`);
+        console.log(`Server running at http://${config.host}:${config.port} all day - everyday!`);
     } catch(err){
         console.log("Startup Error detected - ", err);
     }
