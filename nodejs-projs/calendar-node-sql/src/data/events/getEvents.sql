4b5dc0a735ae47ae2efbb1bc97fa8642154b5925
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-15 11:02:36 -0500

    restruct files and folders

--- nodejs-projs/calendar-node-sql/src/data/events/getEvents.sql
+++
@@ -1,6 +0,0 @@
-
--- To avoid sql injection, always use parameterize queries.
-SELECT [id],[title],[description],[startDate],[startTime],[endDate],[endTime]
-FROM [dbo].[events]
-WHERE [userId] = @userId
-ORDER BY [startDate], [startTime]
--- nodejs-projs/calendar-node-sql/src/plugins/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const sql = require("./sql");
-
-module.exports.register = async function(server) {
-    await server.register(sql);
-}
