d02e2490bbde995a2f345a9c87b47e5ad480d2bf
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-17 15:31:28 -0500

    back to the error

---
+++ js-jq-fundamentals/password-strength/script.js
@@ -0,0 +1,34 @@
+const strengthMeter = document.getElementById("strength-meter");
+const passwordInput = document.getElementById("password-input");
+const reasonsContainer = document.getElementById("reasons");
+
+passwordInput.addEventListener("input", updateStrengthMeter);
+
+function updateStrengthMeter(){
+    const weaknesses = calculatePasswordStrength(passwordInput.value);
+    console.log(weaknesses);
+}
+
+function calculatePasswordStrength(password) {
+    const weaknesses = [];
+    weaknesses.push(lengthWeakness(password))
+    return weaknesses;
+};
+
+function lengthWeakness(password) {
+    const length = password.length;
+
+    if (length <= 5) {
+        return {
+            message: "Your password is too short",
+            deduction: 40
+        }
+    }
+
+    if (length <= 10) {
+        return {
+            message: "Your password could be longer",
+            deduction: 15
+        }
+    }
+}
