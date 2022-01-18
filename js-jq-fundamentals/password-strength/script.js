edc632eff4d271bb1131b9ca15ea0c6f976e53ef
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-18 06:34:46 -0500

    getting the password strenght to calc

--- js-jq-fundamentals/password-strength/script.js
+++ js-jq-fundamentals/password-strength/script.js
@@ -11,34 +11,74 @@ function updateStrengthMeter() {
     let strength = 100;
     reasonsContainer.innerHTML = '';
 
-    weaknesses.forEach( function(weakness) {
+    weaknesses.forEach(function (weakness) {
         if (weakness == null) return;
         strength -= weakness.deduction;
+
+        const messageElement = document.createElement("div");
+        messageElement.innerHTML = weakness.message;
+        reasonsContainer.appendChild(messageElement);
     })
     strengthMeter.style.setProperty('--strength', strength);
 }
 
 function calculatePasswordStrength(password) {
     const weaknesses = [];
-    weaknesses.push(lengthWeakness(password))
+    weaknesses.push(lengthWeakness(password));
+    weaknesses.push(numberWeakness(password));
+    weaknesses.push(lowercaseWeakness(password));
+    weaknesses.push(uppercaseWeakness(password));
     return weaknesses;
 };
 
 function lengthWeakness(password) {
     const length = password.length;
-    if(length <= 0) {return{message:"",deduction:100}}
 
-    if (length <= 5) {
+    if (length <= 4) { return { message: "", deduction: 100 } }
+
+    if (length <= 8) {
         return {
             message: "Your password is too short",
-            deduction: 50
+            deduction: 75
         }
     }
 
-    if (length <= 10) {
+    if (length <= 12) {
         return {
             message: "Your password could be longer",
             deduction: 15
+        }
+    } else return { message: "", deduction: 0 }
+
+}
+
+function lowercaseWeakness(password) {
+    return characterTypeWeakness(password, /[a-z]/g, 'lowercase');
+}
+
+function uppercaseWeakness(password) {
+    return characterTypeWeakness(password, /[A-Z]/g, 'lowercase');
+}
+
+function numberWeakness(password) {
+    return characterTypeWeakness(password, /[0-9]/g, 'numeric');
+}
+
+
+function characterTypeWeakness(password, regex, type) {
+    const matches = password.match(regex) || [];
+
+    if (matches.length === 0) {
+        return {
+            message: `You need at lease two ${type} characters.`,
+            deduction: 20
+        }
+    }
+
+    if (matches.length <= 2) {
+        return {
+            message: `Your password could use more ${type} characters`,
+            deduction: 5
         }
     }
 }
