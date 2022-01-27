45153bef82f5ac8f57f3f6bdbdd54bee03b7a44d
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-26 22:46:49 -0500

    handling the views

--- adv-node-projs/spa-project/frontend/static/js/index.js
+++ adv-node-projs/spa-project/frontend/static/js/index.js
@@ -1,2 +1,45 @@
-alert("JS is loaded");
-console.log("JS is loaded");
+const navigateTo = function (url) {
+    history.pushState(null, null, url);
+    router();
+}
+
+const router = async function () {
+    const routes = [
+        { path: "/", view: function () { console.log("Viewing Dashboard") } },
+        { path: "/posts", view: function () { console.log("Viewing Posts") } },
+        { path: "/settings", view: function () { console.log("Viewing Settings") } },
+    ];
+
+    //Test each route for potential match
+    const potentialMatches = routes.map(function (route) {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    //Find a match
+    let match = potentialMatches.find(p => p.isMatch);
+
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+    // console.log(potentialMatches);
+
+    console.log(match.route.view());
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.addEventListener("click", function (e) {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+    router();
+});
