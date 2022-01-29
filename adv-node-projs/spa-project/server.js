929a4c8cd25111d992c757db359827442e6c0898
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-01-29 13:00:15 -0500

    clientside completed

--- adv-node-projs/spa-project/server.js
+++ adv-node-projs/spa-project/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
 app.get("/*", function(request, response) {
-    response.sendFile(path.resolve("frontend", "index.html"));
+    response.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 app.listen(process.env.PORT || 2022, function(){
