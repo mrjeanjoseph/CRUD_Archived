b0c38bee96dbd5249cad33de9a8c0b631f8efe43
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-02-11 00:08:34 -0500

    media query added

--- js-jq-fundamentals/foodtrucksite/script.js
+++ js-jq-fundamentals/foodtrucksite/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     $('.fa-hamburger').click(function(){
-        //$(this).toggleClass('fa-times');
+        $(this).toggleClass('fa-times');
         $('nav').toggleClass('nav-toggle');
     });
+    $('nav ul li a').click(function(){
+        $('.fa-hamburger').removeClass('fa-times');
+        $('nav').removeClass('nav-toggle');
+    })
 });
