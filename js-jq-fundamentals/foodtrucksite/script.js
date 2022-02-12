accf795d3b53d5342ffd51d2e7da88d4ff667b27
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-02-12 15:52:22 -0500

    creating the special section

--- js-jq-fundamentals/foodtrucksite/script.js
+++ js-jq-fundamentals/foodtrucksite/script.js
@@ -7,5 +7,20 @@ $(document).ready(function(){
     $('nav ul li a').click(function() {
         $('.fa-drumstick-bite').removeClass('fa-times');
         $('nav').removeClass('nav-toggle');
-    })
+    });
+    $('.dot1').click(function(){
+        $('.vid1').css('display', 'block');
+        $('.vid2').css('display', 'none');
+        $('.vid3').css('display', 'none');
+    });
+    $('.dot2').click(function(){
+        $('.vid1').css('display', 'none');
+        $('.vid2').css('display', 'block');
+        $('.vid3').css('display', 'none');
+    });
+    $('.dot3').click(function(){
+        $('.vid1').css('display', 'none');
+        $('.vid2').css('display', 'none');
+        $('.vid3').css('display', 'block');
+    });
 })
