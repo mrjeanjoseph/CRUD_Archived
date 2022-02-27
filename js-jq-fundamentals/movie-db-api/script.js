9c6be139617265f89340af7b248501005e8c7104
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-02-27 09:53:12 -0500

    targeting the dom

---
+++ js-jq-fundamentals/movie-db-api/script.js
@@ -0,0 +1,19 @@
+const searchPage = document.querySelector('#search-page');
+const searchResult = document.querySelector('#search-result');
+const btn = document.querySelector('#btn');
+const form = document.querySelector('#form');
+
+const search = document.querySelector('#search');
+const results = document.querySelector('#results');
+const offlinePage = document.querySelector('#offline-page');
+const searchTitle = document.querySelector('#search-title');
+const message = document.querySelector('#message');
+
+const detailsPage = document.querySelector('#details-page');
+const cardName = document.querySelector('#card-name');
+const title = document.querySelector('#title');
+const rating = document.querySelector('#rating');
+const date = document.querySelector('#date');
+const img = document.querySelector('#card-img');
+const description = document.querySelector('#description');
+const body = document.querySelector('#body');
