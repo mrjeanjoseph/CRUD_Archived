832bc3a25c2ecf72b226ea2551352e3688e6965b
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-06-25 17:35:35 -0400

    leap year challenge

--- DSA-Rehearsal/MainConsoleUI/Program.cs
+++ DSA-Rehearsal/MainConsoleUI/Program.cs
@@ -6,7 +6,42 @@ namespace MainConsoleUI
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                
+                try
+                {
+                    LeapYear();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Jean found an error: \n" + e.Message);
+                }
+                Console.ReadLine();
+            }
+
+
+        }
+
+        static void LeapYear()
+        {
+            int year;
+            string response;
+            Console.WriteLine("Enter a Year:");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                response = "Year is leap";
+            }
+            else
+            {
+                response = "Not a leap year";
+            }
+            //response = year % 4 == 0 ? "Year is leap year." : "Year is not a leap year.";
+            Console.WriteLine(response);
         }
     }
+
+
 
 }
