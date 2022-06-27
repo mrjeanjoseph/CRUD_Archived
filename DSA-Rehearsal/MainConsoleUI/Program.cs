0715d89ddafaa35f2031f1d182211b4349dbd06f
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-06-27 09:52:27 -0400

    BubbleSort challenge

--- DSA-Rehearsal/MainConsoleUI/Program.cs
+++ DSA-Rehearsal/MainConsoleUI/Program.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Net.WebSockets;
 
-namespace MainConsoleUI
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                try
-                {
-                    SortASCOrder();
+namespace MainConsoleUI {
+    class Program {
+        static void Main(string[] args) {
+            while (true) {
+                try {
+                    BubbleSorting();
+                    //SortASCOrder();
                     //CountPrintExecution();
                     //ShowPrimeNumbers();
                     //DuplicateCount();
@@ -20,9 +16,7 @@ namespace MainConsoleUI
                     //Iteration();
                     //IdentityMatrix();
                     //LeapYear();
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     Console.WriteLine("Jean found an error: \n" + e.Message);
                 }
                 Console.ReadLine();
@@ -30,29 +24,54 @@ namespace MainConsoleUI
         }
 
 
-        public static void SortASCOrder()
-        {
+        public static void BubbleSorting() {
+            int num, temp;
+
+            Console.WriteLine("Enter the numbers you want to enter: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[num];
+            for (int i = 0; i < num; i++) {
+                Console.WriteLine("Enter " + i + " value");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < num; i++) {
+                for (int j = 0; j < num - 1; j++) {
+                    if (array[i] == array[j + 1]) {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("In Ascending order using bubble sort: ");
+            for(int i = 0; i < num; i++) {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public static void SortASCOrder() {
             //Instruction: Takes nth values from user and then sorts them in ascending order.
             int num, temp;
             string nth = "";
+
             Console.Write("How many numerical values to sort? ");
             num = Convert.ToInt32(Console.ReadLine());
+
             int[] array = new int[num];
 
-            for (int x = 0; x < num; x++)
-            {
-                if (x+1 == 1)
-                {
+            for (int x = 0; x < num; x++) {
+                if (x + 1 == 1) {
                     nth = "First";
                     Console.Write($"Enter the {nth} value: ");
                 }
-                if (x+1 == 2)
-                {
+                if (x + 1 == 2) {
                     nth = "Second";
                     Console.Write($"Enter the {nth} value: ");
                 }
-                if (x+1 >= 3)
-                {
+                if (x + 1 >= 3) {
                     nth = "th";
                     Console.Write($"Enter the {x}{nth} value: ");
                 }
@@ -60,12 +79,9 @@ namespace MainConsoleUI
                 array[x] = Convert.ToInt32(Console.ReadLine());
             }
 
-            for (int x = 0; x < num; x++)
-            {
-                for (int y = 0; y < num - 1; y++)
-                {
-                    if (array[y] > array[y + 1])
-                    {
+            for (int x = 0; x < num; x++) {
+                for (int y = 0; y < num - 1; y++) {
+                    if (array[y] > array[y + 1]) {
                         temp = array[y];
                         array[y] = array[y + 1];
                         array[y + 1] = temp;
@@ -74,31 +90,23 @@ namespace MainConsoleUI
             }
 
             Console.WriteLine("In ASC order..,");
-            for (int x = 0; x < num; x++)
-            {
+            for (int x = 0; x < num; x++) {
                 Console.WriteLine(array[x]);
             }
 
         }
 
-        public static void CountPrintExecution()
-        {
+        public static void CountPrintExecution() {
             int x, y, z, count = 0;
-            for (x = 0; x <= 3; x++)
-            {
+            for (x = 0; x <= 3; x++) {
 
-                for (y = 0; y <= 3; y++)
-                {
+                for (y = 0; y <= 3; y++) {
 
-                    for (z = 0; z <= 3; z++)
-                    {
+                    for (z = 0; z <= 3; z++) {
 
-                        if (x == 3 && y == 3 && z == 3)
-                        {
+                        if (x == 3 && y == 3 && z == 3) {
                             break;
-                        }
-                        else
-                        {
+                        } else {
                             //This needs to be reworked
                             count++;
                             Console.WriteLine("X " + x + "Y " + y + "Z " + z);
@@ -109,50 +117,39 @@ namespace MainConsoleUI
             Console.WriteLine("In this program, writeline will print " + count + " times.");
         }
 
-        public static void ShowPrimeNumbers()
-        {
+        public static void ShowPrimeNumbers() {
             int[] array = new int[10];
             int count = 0, i, j, k = 2;
             Console.WriteLine("Enter up to 10 values:");
-            for (i = 0; i < 10; i++)
-            {
+            for (i = 0; i < 10; i++) {
                 Console.Write(i + 1 + ": ");
                 array[i] = Convert.ToInt32(Console.ReadLine());
             }
-            for (j = 0; j < 10; j++)
-            {
-                for (k = 2; j < array[count]; k++)
-                {
-                    if (array[count] % k == 0)
-                    {
+            for (j = 0; j < 10; j++) {
+                for (k = 2; j < array[count]; k++) {
+                    if (array[count] % k == 0) {
                         goto to;
                     }
                 }
-            to: if (k == array[count])
-                {
+            to: if (k == array[count]) {
                     Console.WriteLine("Entered value: " + array[count] + " is prime number");
                 }
-                if (array[count] == 0 || array[count] == 1)
-                {
+                if (array[count] == 0 || array[count] == 1) {
                     Console.WriteLine("Entered value : " + array[count] + " is not prime number");
                 }
                 count++;
             }
         }
 
-        public static void DuplicateCount()
-        {
+        public static void DuplicateCount() {
             int space = 7;
             int num = 1;
-            for (int x = 1; x < 5; x++)
-            {
-                for (int y = 1; y <= space; y++)
-                {
+            for (int x = 1; x < 5; x++) {
+                for (int y = 1; y <= space; y++) {
                     Console.Write(" ");
                 }
                 space = space - 2;
-                for (int z = 1; z <= x; z++)
-                {
+                for (int z = 1; z <= x; z++) {
                     Console.Write(num);
                     Console.Write(" ");
                     num++;
@@ -162,8 +159,7 @@ namespace MainConsoleUI
             Console.ReadLine();
         }
 
-        public static void PrintASCIIValues()
-        {
+        public static void PrintASCIIValues() {
             //Write a program to print all the ASCII values and their equivalent characters using a while loop.
             //The ASCII values vary from 0 to 255.
             char ch;
@@ -181,8 +177,7 @@ namespace MainConsoleUI
             Console.ReadLine();
         }
 
-        public static void PrintOutput()
-        {
+        public static void PrintOutput() {
             //Write a program using a loop that prints the following output.
             //1 2 2 3 3 3 4 4 4 4 5 5 5 5 5 6 6 6 6 6 6. . .nth iteration.
             int value;
@@ -191,10 +186,8 @@ namespace MainConsoleUI
             value = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("\n");
 
-            for (int x = 0; x <= value; x++)
-            {
-                for (int y = 1; y <= x; y++)
-                {
+            for (int x = 0; x <= value; x++) {
+                for (int y = 1; y <= x; y++) {
                     Console.Write(x);
                 }
                 Console.Write(" ");
@@ -202,8 +195,7 @@ namespace MainConsoleUI
             Console.ReadLine();
         }
 
-        public static void Iteration()
-        {
+        public static void Iteration() {
             //Write a program using a for loop that prints the following series.
             //1 2 4 8 16 21 64 128 …nth iteration.
             int value;
@@ -213,8 +205,7 @@ namespace MainConsoleUI
             value = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Here are the iteration: ");
 
-            for (int x = 0; x <= value; x++)
-            {
+            for (int x = 0; x <= value; x++) {
                 result = Math.Pow(2, x);
                 Console.Write(result);
                 Console.Write(" ");
@@ -222,8 +213,7 @@ namespace MainConsoleUI
             Console.ReadKey();
         }
 
-        public static void IdentityMatrix()
-        {
+        public static void IdentityMatrix() {
             //Write a program that prints an identity matrix using a for loop,
             //in other words takes a value n from the user and shows the identity table of size n * n.
             int size;
@@ -231,16 +221,11 @@ namespace MainConsoleUI
             size = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Identity Matrix");
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    if (i == j)
-                    {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (i == j) {
                         Console.Write(1);
-                    }
-                    else
-                    {
+                    } else {
                         Console.Write(0);
                     }
                 }
@@ -249,8 +234,7 @@ namespace MainConsoleUI
             Console.ReadLine();
         }
 
-        public static void LeapYear()
-        {
+        public static void LeapYear() {
             //Instruction:
             //Write a program using conditional operators to determine whether a year entered through the keyboard is a leap year or not.
 
@@ -258,12 +242,9 @@ namespace MainConsoleUI
             string response;
             Console.WriteLine("Enter a Year:");
             year = Convert.ToInt32(Console.ReadLine());
-            if (year % 4 == 0)
-            {
+            if (year % 4 == 0) {
                 response = "Year is leap";
-            }
-            else
-            {
+            } else {
                 response = "Not a leap year";
             }
             //I can use a Ternary operation too
