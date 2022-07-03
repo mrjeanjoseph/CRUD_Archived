fa49ef57d017079ac24d80e26aac7c196d377788
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-07-03 08:43:38 -0400

    sql challenges HR

--- SQL-Rehearsal/orders-table.sql
+++ SQL-Rehearsal/orders-table.sql
@@ -42,3 +42,49 @@ ADD CONSTRAINT FK_EmpDept FOREIGN KEY (DepartmentID) REFERENCES Departments(Dept
 DROP CONSTRAINT FK__Employees__Manag__4D94879B;
 
 SELECT * FROM Employees;
+
+--Creating customer's table
+CREATE TABLE Customers(
+	CustomerID INT NOT NULL IDENTITY(7,7),
+	CustFirstName VARCHAR(35) NOT NULL,
+	CustLastName VARCHAR(35) NOT NULL,
+	EmailAddress VARCHAR(100) NOT NULL,
+	PhoneNumber VARCHAR(11),
+	PrefContact VARCHAR(10) NOT NULL,
+	PRIMARY KEY(CustomerID)
+);
+
+INSERT INTO Customers ([CustFirstName],[CustLastName],[EmailAddress],[PhoneNumber],[PrefContact])
+VALUES
+	('Jacques','Sauveur','jacques.s@ht-email.com','5093581122','Email'),
+	('Evelyn','Michelle','e.michelle@ht-email.com','5094483985','Phone'),
+	('Nixon','Paniague','n.paniague@ht-email.com','5091520012','Phone'),
+	('Calvin','Jouthe','cjouthe@us-email.com','5098784552','Email'),
+	('Rachelle','Charles','r.charles@us-email.com','5905225887','Phone')
+
+SELECT * FROM Customers;
+
+--Creating product table
+
+CREATE TABLE Cars (
+	CarsID INT NOT NULL IDENTITY(1205,3),
+	CustomerID INT NOT NULL,
+	EmployeeID INT NOT NULL,
+	Model VARCHAR(50) NOT NULL,
+	ProdStatus VARCHAR(25) NOT NULL,
+	TotalCost INT NOT NULL,
+	PRIMARY KEY(CarsID),
+	FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+	FOREIGN KEY (EmployeeID) REFERENCES Employees(EmpID)
+);
+
+SELECT * FROM Customers;
+SELECT * FROM Employees;
+SELECT * FROM Cars;
+INSERT INTO Cars ([CustomerID],[EmployeeID],[Model],[ProdStatus],[TotalCost])
+VALUES
+	(7,566,'Toyota Tundra','READY','75'),
+	(35,578,'Cadillac Escalade','PENDING','112'),
+	(14,569,'Tesla Cyber Trunk','PENDING','186'),
+	(21,575,'Chevy Avalance','READY','69'),
+	(28,572,'Nissa Armada','READY','81');
