08bec36e3ea634b07ecfbcd84c7731e070c2a9eb
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-07-03 12:25:33 -0400

    Using short Aliases

---
+++ SQL-Rehearsal/create logins.sql
@@ -0,0 +1,7 @@
+-- Creates logins
+    WITH PASSWORD = '';  
+GO  
+
+-- Creates a database user for the login created above.  
+CREATE USER FrednelJeanJoseph FOR LOGIN fjeanjoseph; 
+GO
---
+++ SQL-Rehearsal/short-aliases.sql
@@ -0,0 +1,28 @@
+SELECT Employees.*,Departments.DeptName 'Dept Name'
+FROM Employees
+JOIN Departments
+ON Departments.DeptID = Employees.DepartmentID;
+--Pick up from here!
+
+
+SELECT 
+	Customers.CustFirstName+' '+Customers.CustLastName AS 'Customer Name',
+	Cars.CarsID AS 'Customer ID',
+	Customers.PhoneNumber,
+	Customers.EmailAddress
+	
+FROM 
+	Customers
+LEFT JOIN
+	Cars ON Cars.CustomerID = Customers.CustomerID
+
+--Using short Aliases
+SELECT 
+	c.CustFirstName+' '+c.CustLastName AS [Customer Name],
+	v.CarsID AS 'Customer ID',
+	c.PhoneNumber,
+	c.EmailAddress
+FROM Customers c
+LEFT JOIN 
+	Cars as v ON v.CustomerID = c.CustomerID
+ORDER BY [Customer Name]
