a38d9da34b3ad80c904574aecea00f194dfe4a25
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-07-04 11:20:57 -0400

    DSA rehearsals

--- DSA-Rehearsal/BasicDSAChallenges/Program.cs
+++ DSA-Rehearsal/BasicDSAChallenges/Program.cs
@@ -26,24 +26,36 @@ namespace BasicDSAChallenges {
 
         public static void runProgram() {
 
-            FindLongestWord();
+            //FindLongestWord();
             //Console.WriteLine(Within20or100(221));
             //Console.WriteLine(ReturnTrueIf20(5,15));
             //Console.WriteLine(ReturnDoubleAbsValues(10,7));           
             //Console.WriteLine(TripleSum(6,3));
             //Console.WriteLine(TripleSum(5,5));
 
+            PrintOddNumbers();
+
+        }
+
+        private static void PrintOddNumbers() {
+            //Print odd numbers from 1 to 99 - one number per line
+            for (int i = 1; i < 100; i++) {
+                if (i % 2 != 0) {
+
+                    Console.WriteLine(i);
+                }
+            }
         }
 
         public static void FindLongestWord() {
             //Find the longest word in a string
             string str = "finding the longest word in a complicated string.";
 
-            string[] words = str.Split(new[] {" "}, StringSplitOptions.None);
+            string[] words = str.Split(new[] { " " }, StringSplitOptions.None);
             string word = "";
             int counter = 0;
-            foreach(string s in words) {
-                if(s.Length > counter) {
+            foreach (string s in words) {
+                if (s.Length > counter) {
                     word = s;
                     counter = s.Length;
                 }
@@ -56,8 +68,9 @@ namespace BasicDSAChallenges {
             if (Math.Abs(x - 100) <= 20 || Math.Abs(x - 200) <= 20)
                 return true;
             return false;
-            
+
         }
+
         public static bool ReturnTrueIf20(int x, int y) {
             //Check the sum of the two given integers and return true if one of the integer is 20 or if their sum is 20.
             return x == 20 || y == 20 || (x + y == 20);
@@ -65,7 +78,7 @@ namespace BasicDSAChallenges {
 
         public static int ReturnDoubleAbsValues(int x, int y) {
             //Get the absolute value of the difference between two given numbers. Return double the absolute value of the difference if the first number is greater than second number.
-            if(x > y)
+            if (x > y)
                 return (x - y) * 2;
             else
                 return (x - y);
