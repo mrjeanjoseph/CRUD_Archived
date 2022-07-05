cd76f8329cab6222275627cd00868f59ee9db68c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-07-05 06:37:15 -0400

    order by queries

---
+++ SQL-Rehearsal/Ch8-OrderByQuery.sql
@@ -0,0 +1,26 @@
+SELECT *
+FROM production.BillOfMaterials;
+
+SELECT * FROM production.ProductCostHistory;
+
+SELECT productID, COUNT(*) [Standard Cost] FROM production.ProductCostHistory
+GROUP BY ProductID
+ORDER BY [Standard Cost] DESC
+
+SELECT bp.BillOfMaterialsID,bp.ComponentID
+FROM Production.BillOfMaterials bp
+GROUP BY bp.ComponentID, bp.BillOfMaterialsID
+
+SELECT ppv.ProductID, ppv.OnOrderQty FROM Purchasing.ProductVendor ppv ORDER BY 2;
+
+--Use ORDER BY with TOP
+SELECT * FROM Purchasing.PurchaseOrderHeader
+SELECT TOP 5 PurchaseOrderID, VendorID FROM Purchasing.PurchaseOrderHeader
+SELECT TOP 5 PurchaseOrderID, VendorID 
+FROM Purchasing.PurchaseOrderHeader
+ORDER BY VendorID DESC
+
+--Customize sorting order
+SELECT * FROM Sales.Store;
+SELECT * FROM Sales.SpecialOfferProduct;
+SELECT * FROM Sales.CountryRegionCurrency;
