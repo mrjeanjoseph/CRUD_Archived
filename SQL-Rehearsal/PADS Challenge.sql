0556fb45e28353b496587f5724f23ae798c422dd
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-07-06 17:43:22 -0400

    rehearse sql

--- SQL-Rehearsal/PADS Challenge.sql
+++
@@ -1,5 +0,0 @@
-SELECT NAME +'('+LEFT(OCCUPATION,1)+')' FROM OCCUPATIONS ORDER BY name ASC;
-
-SELECT 'There are a total of '+rtrim(CAST(COUNT(occupation) AS CHAR))+' '+LOWER(Occupation)+'s.' FROM OCCUPATIONS GROUP BY OCCUPATION ORDER BY COUNT(name) ASC, occupation ASC;
-
---https://www.hackerrank.com/challenges/the-pads/problem?isFullScreen=true
--- SQL-Rehearsal/create logins.sql
+++
@@ -1,7 +0,0 @@
--- Creates logins
-    WITH PASSWORD = '';  
-GO  
-
--- Creates a database user for the login created above.  
-CREATE USER FrednelJeanJoseph FOR LOGIN fjeanjoseph; 
-GO
