027cd80ca280f3aef043d33444413ccd9805ccc5
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-07-30 16:27:35 -0400

    c-sharp sql connect

--- DSA-Rehearsal/ConsoleDB_Rehearse/Program.cs
+++ DSA-Rehearsal/ConsoleDB_Rehearse/Program.cs
@@ -1,25 +1,82 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace ConsoleDB_Rehearse {
     class Program {
+
         static void Main(string[] args) {
-            ConnectToDB2();
             Console.ReadLine();
+
+            GetEmployeeInfo(args);
         }
 
-        public static void ConnectToDB2() {
-            string connString = @"Server=.\sqlexpress;Database=master;Trusted_Connection=True;";
+        public static void GetEmployeeInfo(string[] args) {
+            string connString = @"Server=(localdb)\MSSQLLocalDB;Database=master;Trusted_Connection=True;";
+            int empID;
+            string empCode, empFirstName, empLastName, locCode, locDesc;
 
             try {
-                using SqlConnection conn = new SqlConnection(connString); 
+                using (SqlConnection conn = new SqlConnection(connString)) {
+                    string spName = @"[SCHEMA_TESTING1].[uspEmployeeInfo]";
+
+                    SqlCommand cmd = new SqlCommand(spName, conn);
+
+                    SqlParameter param1 = new SqlParameter();
+                    param1.ParameterName = "@ID";
+                    param1.SqlDbType = SqlDbType.Int;
+                    param1.Value = int.Parse(args[0].ToString());
+
+                    cmd.Parameters.Add(param1);
+
+                    conn.Open();
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Console.WriteLine(Environment.NewLine + "Reatrieving data from database...," + Environment.NewLine);
+                    Console.WriteLine("Retrieving record");
+
+                    if (reader.HasRows) {
+
+                        while (reader.Read()) {
+                            empID = reader.GetInt32(0);
+                            empCode = reader.GetString(1);
+                            empFirstName = reader.GetString(2);
+                            empLastName = reader.GetString(3);
+                            locCode = reader.GetString(4);
+                            locDesc = reader.GetString(5);
+
+                            Console.WriteLine($"{empID}, {empCode}, {empFirstName}, {empLastName}, {locCode}, {locDesc}");
+                        }
+                    } else {
+                        Console.WriteLine("No data Found...,");
+                    }
+
+                    reader.Close();
+                    conn.Close();
+
+                }
+
+            } catch (Exception ex) {
+
+                Console.WriteLine("OOOPS! -" + ex.Message);
+            }
+        }
+
+        public static void CheckConnectionToDB() {
+            //The entire server name needs to be inserted to connect successfully. (localdb)\MSSQLLocalDB
+            string connString = @"Server=(localdb)\MSSQLLocalDB;Database=master;Trusted_Connection=True;";
+
+            try {
+                using SqlConnection conn = new SqlConnection(connString);
                 string query = @"SELECT @@VERSION";
 
                 SqlCommand cmd = new SqlCommand(query, conn);
                 conn.Open();
                 SqlDataReader reader = cmd.ExecuteReader();
 
-                if (reader.Read()) {
+                if (reader.HasRows) {
                     while (reader.Read()) {
                         Console.WriteLine(reader.GetString(0));
                     }
