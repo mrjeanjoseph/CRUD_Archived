9d86de257917dd331e6bb28b3e1230355f678f7c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-07-31 10:04:09 -0400

    ef-db rehearse

---
+++ DSA-Rehearsal/EFCodeFirst_Rehearse/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EFCodeFirst_Rehearse {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
