9fb594d8dfb21e47b855db295eb1ffcd7955d90e
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-09-04 13:54:34 -0400

    utc date times

---
+++ DSA/DateTime/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace DateTimeReheasals {
+    class Program {
+        static void Main(string[] args) {
+
+            DisplayUTCNowDates();
+            Console.ReadLine();
+        }
+
+        public static void DisplayUTCNowDates() {
+            DateTime localDate = DateTime.Now;
+            DateTime utcDate = DateTime.UtcNow;
+            String[] cultureNames = { "en-US", "en-GB", "fr-FR",
+                                "de-DE", "ru-RU" };
+
+            foreach (var cultureName in cultureNames) {
+                var culture = new CultureInfo(cultureName);
+                Console.WriteLine("{0}:", culture.NativeName);
+                Console.WriteLine("   Local date and time: {0}, {1:G}",
+                                  localDate.ToString(culture), localDate.Kind);
+                Console.WriteLine("   UTC date and time: {0}, {1:G}\n",
+                                  utcDate.ToString(culture), utcDate.Kind);
+            }
+        }
+
+        public static void DisplayDateTimeValue() {
+            DateTime localDate = DateTime.Now;
+
+            DateTime dt1 = new DateTime(2016, 6, 8, 11, 49, 0);
+            Console.WriteLine("Complete date: " + dt1.ToString());
+
+            // Get date-only portion of date, without its time.
+            DateTime dateOnly = dt1.Date;
+
+            Console.WriteLine("Short Date: " + dateOnly.ToString("d"));
+
+            Console.WriteLine("Display date using 24-hour clock format:");
+
+            Console.WriteLine(dateOnly.ToString("g"));
+            Console.WriteLine(dateOnly.ToString("MM/dd/yyyy HH:mm"));
+        }
+
+        
+    }
+}
