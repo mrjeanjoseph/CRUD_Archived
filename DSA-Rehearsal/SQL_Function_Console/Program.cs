244439e46608dcc4492069e1e5e22aa496ebbd4c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-09-04 10:06:04 -0400

    restructure files and folders

--- DSA-Rehearsal/SQL_Function_Console/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SQL_Function_Console {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
