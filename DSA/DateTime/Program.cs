167cb56df7c81e4a9a7aa592844403a90a357def
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-09-05 10:49:03 -0400

    current to local time

--- DSA/DateTime/Program.cs
+++ DSA/DateTime/Program.cs
@@ -5,7 +5,7 @@ namespace DateTimeReheasals {
     class Program {
         static void Main(string[] args) {
 
-            DiffBetweenTwoDates();
+            CurrentDateTimeObjectToLocalTime();
             Console.ReadLine();
         }
 
@@ -265,6 +265,40 @@ namespace DateTimeReheasals {
 
             //Extract days, write to Console.
             Console.WriteLine("Difference in days: " + difference.Days);
+        }
+
+        public static void CurrentDateTimeObjectToLocalTime() {
+            DateTime localDateTime, univDateTime;
+
+            Console.WriteLine("Enter a date and time.");
+            string strDateTime = Console.ReadLine();
+
+            try {
+                localDateTime = DateTime.Parse(strDateTime);
+                univDateTime = localDateTime.ToUniversalTime();
+
+                Console.WriteLine("{0} local time is {1} universal time.",
+                                       localDateTime,
+                                        univDateTime);
+            } catch (FormatException) {
+                Console.WriteLine("Invalid format.");
+                return;
+            }
+
+            Console.WriteLine("Enter a date and time in universal time.");
+            strDateTime = Console.ReadLine();
+
+            try {
+                univDateTime = DateTime.Parse(strDateTime);
+                localDateTime = univDateTime.ToLocalTime();
+
+                Console.WriteLine("{0} universal time is {1} local time.",
+                                         univDateTime,
+                                         localDateTime);
+            } catch (FormatException) {
+                Console.WriteLine("Invalid format.");
+                return;
+            }
         }
     }
 }
