de404e90147f33fcde06ab3c4b1f32f7f3339d31
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-09-07 08:28:56 -0400

    struct public/private rehearse

--- DSA/StructureRehearsals/ExerciseOneStruct.cs
+++ DSA/StructureRehearsals/ExerciseOneStruct.cs
@@ -8,4 +8,18 @@
         public static int x = 28;
         public static int y = 52;
     }
+
+    class ExerciseThreeStruct {
+        //Employee is to be a structure members
+        public struct employee {
+            public string empName;
+            public BirthDate Date;                
+        }
+
+        public struct BirthDate {
+            public int Day;
+            public int Month;
+            public int Year;
+        }
+    }
 }
--- DSA/StructureRehearsals/Program.cs
+++ DSA/StructureRehearsals/Program.cs
@@ -1,11 +1,43 @@
 ﻿using System;
+using static StructureRehearsals.ExerciseThreeStruct;
 
 namespace StructureRehearsals {
     class Program {
         static void Main(string[] args) {
 
-            ExerciseTwo();
+            ExerciseThree();
             Console.ReadLine();
+        }
+
+        public static void ExerciseThree() {
+            string employeeName = "";
+            int birthDay = 0, birthMonth = 0, birthYear = 0, total = 1;
+            var getCurrYear = DateTime.Now.ToString("yy");
+            //DateTime getCurrYear = DateTime.Now;
+            //getCurrYear.ToString("yy");
+            Console.WriteLine(getCurrYear);
+
+            Console.WriteLine("Declaring a nested structure and store data in an array: ");
+            employee[] emp = new employee[total];
+
+            for (int x = 0; x < total; x++) {
+                Console.WriteLine("Name of the employee: ");
+                employeeName = Console.ReadLine();
+                emp[x].empName = employeeName;
+
+                Console.WriteLine("Enter the birth date of the employee: ");
+                birthDay = Convert.ToInt32(Console.ReadLine());
+                emp[x].Date.Day = birthDay;
+
+                Console.WriteLine("Enter the birth Month of the employee: ");
+                birthMonth = Convert.ToInt32(Console.ReadLine());
+                emp[x].Date.Month = birthMonth;
+
+                Console.WriteLine("Enter the year of the employee: ");
+                birthYear = Convert.ToInt32(Console.ReadLine());
+                emp[x].Date.Year = birthYear;
+            }
+            Console.WriteLine($"Employee: {employeeName} \n \tDate of Birth: {birthMonth}/{birthDay}/{birthYear}");
         }
 
         public static void ExerciseTwo() {
