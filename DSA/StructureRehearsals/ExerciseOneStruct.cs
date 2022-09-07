1a19782c90ce615b05a82193bbd73e5cc230a951
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-09-07 07:57:45 -0400

    rehearsing static structs

---
+++ DSA/StructureRehearsals/ExerciseOneStruct.cs
@@ -0,0 +1,11 @@
+﻿namespace StructureRehearsals {
+    struct ExerciseOneStruct {
+        public int x;
+        public int y;
+    }
+
+    struct ExerciseTwoStruct {
+        public static int x = 28;
+        public static int y = 52;
+    }
+}
--- DSA/StructureRehearsals/Program.cs
+++ DSA/StructureRehearsals/Program.cs
@@ -3,7 +3,28 @@
 namespace StructureRehearsals {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            ExerciseTwo();
+            Console.ReadLine();
+        }
+
+        public static void ExerciseTwo() {
+            Console.WriteLine("Declaring a structure with the use of static fields:");
+            Console.WriteLine("====================================================");
+
+            int result = ExerciseTwoStruct.x + ExerciseTwoStruct.y;
+            Console.WriteLine($"The sum of x and y using static struct is: {result}");
+        }
+
+        public static void ExerciseOne() {
+            Console.WriteLine("Declaring a simple struct: ");
+            Console.WriteLine("===========================");
+
+            ExerciseOneStruct exstruct = new ExerciseOneStruct();
+            exstruct.x = 71;
+            exstruct.y = 29;
+            int xytotal = exstruct.x + exstruct.y;
+            Console.WriteLine($"The sum of x and y is {xytotal}");
         }
     }
 }
