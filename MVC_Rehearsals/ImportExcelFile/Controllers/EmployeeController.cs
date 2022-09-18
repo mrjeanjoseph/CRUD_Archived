bb6eaf7ca3ac945666405fb68c6def745097a4f7
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-09-18 12:18:37 -0400

    update completed

---
+++ MVC_Rehearsals/ImportExcelFile/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ImportExcelFile.Controllers {
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDbContext _dbContext;
+
+        public EmployeeController() {
+            _dbContext = new EmployeeDbContext();
+        }
+
+        // GET: Student  
+        public ActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ImportFile() {
+            return View("Index");
+        }
+    }
+}
