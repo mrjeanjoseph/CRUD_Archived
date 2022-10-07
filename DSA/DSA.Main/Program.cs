18d2c77e0515ede980a4dc88ccabae3254082b0c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-10-07 19:55:40 -0400

    last update

--- DSA/DSA.Main/Program.cs
+++ DSA/DSA.Main/Program.cs
@@ -3,12 +3,19 @@
 namespace DSA.Main {
     class Program {
         static void Main(string[] args) {
-            DSA mydsa = new DSA(001,"ValueOne","A0045B5-6XX",Convert.ToDateTime("04/06/1997"));
+
+            DateTime myDate = DateTime.Now;
+            Console.WriteLine(myDate);
+            Console.ReadLine();
+        }
+
+        public static void MyDSA() {
+
+            DSA mydsa = new DSA(001, "ValueOne", "A0045B5-6XX", Convert.ToDateTime("04/06/1997"));
 
             Console.WriteLine("My DSA ID: " + mydsa.dsaID);
             Console.WriteLine("My DSA Name: " + mydsa.dsaName);
             Console.WriteLine("My DSA AltID: " + mydsa.dsaAltID);
-            Console.ReadLine();
         }
     }
 }
