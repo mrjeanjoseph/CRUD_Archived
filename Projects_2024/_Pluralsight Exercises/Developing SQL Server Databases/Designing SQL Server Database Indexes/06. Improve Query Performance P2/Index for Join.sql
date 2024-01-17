05826e4d9c2c7ad40c524ba5456b844897f61792
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-01-17 06:41:47 -0500

    impr qry pfrmc done

---
+++ Projects_2024/_Pluralsight Exercises/Developing SQL Server Databases/Designing SQL Server Database Indexes/06. Improve Query Performance P2/Index for Join.sql
@@ -0,0 +1,17 @@
+USE [InterstellarTransport]
+
+SELECT ss.[Location], sh.ShipmentID
+FROM dbo.Stations ss
+INNER JOIN dbo.Shipments sh ON sh.OriginStationID = ss.StationID
+WHERE ss.[Location] = 'Outer Transfer';
+
+SELECT ss.[Location], sh.ShipmentID
+FROM dbo.Stations ss
+INNER MERGE JOIN dbo.Shipments sh ON sh.OriginStationID = ss.StationID
+WHERE ss.[Location] = 'Outer Transfer';
+
+--
+USE [InterstellarTransport]
+GO
+CREATE NONCLUSTERED INDEX idx_test
+ON [dbo].[Shipments] (OriginStationID)
